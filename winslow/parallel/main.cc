7413e7728081d7c69cbee8a176455498764308f4
Praveen C <cpraveen@gmail.com>
2016-06-24 11:40:48 +0530

    Catch exceptions

--- winslow/parallel/main.cc
+++ winslow/parallel/main.cc
@@ -1,13 +1,12 @@
 /* ---------------------------------------------------------------------
-Solve the Winslow equations using Picard iterations as in
-   Meire Fortunato, Per-Olof Persson
-   High-order unstructured curved mesh generation using the Winslow equations
-   Journal of Computational Physics 307 (2016) 1–14
-   http://dx.doi.org/10.1016/j.jcp.2015.11.020
---------------------------------------------------------------------- */
+ Solve the Winslow equations using Picard iterations as in
+ Meire Fortunato, Per-Olof Persson
+ High-order unstructured curved mesh generation using the Winslow equations
+ Journal of Computational Physics 307 (2016) 1–14
+ http://dx.doi.org/10.1016/j.jcp.2015.11.020
+ --------------------------------------------------------------------- */
 
 
-#include <deal.II/grid/tria.h>
 #include <deal.II/grid/grid_generator.h>
 #include <deal.II/grid/manifold.h>
 #include <deal.II/grid/manifold_lib.h>
@@ -23,63 +22,89 @@ Solve the Winslow equations using Picard iterations as in
 //------------------------------------------------------------------------------
 int main (int argc, char *argv[])
 {
-   using namespace dealii;
-   using namespace Winslow;
-   
-   Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv);
-
-   // Setup the triangulation
-   parallel::distributed::Triangulation<2> triangulation (MPI_COMM_WORLD);
-   
-   unsigned int test_case = 1;
-   unsigned int n_refine = 0;
-   
-   if(test_case==0)
+   try
    {
-      GridGenerator::hyper_ball (triangulation);
-      n_refine = 2;
+      using namespace dealii;
+      using namespace Winslow;
+      
+      Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv);
+      
+      // Setup the triangulation
+      parallel::distributed::Triangulation<2> triangulation (MPI_COMM_WORLD);
+      
+      unsigned int test_case = 1;
+      unsigned int n_refine = 0;
+      
+      if(test_case==0)
+      {
+         GridGenerator::hyper_ball (triangulation);
+         n_refine = 2;
+      }
+      else if(test_case==1)
+      {
+         GridGenerator::hyper_shell (triangulation, Point<2>(0.0,0.0), 0.5, 1.0);
+         n_refine = 2;
+      }
+      else if(test_case==2)
+      {
+         GridIn<2> grid_in;
+         grid_in.attach_triangulation(triangulation);
+         std::ifstream input_file("annulus.msh");
+         grid_in.read_msh(input_file);
+         n_refine = 0;
+      }
+      else if(test_case==3)
+      {
+         GridIn<2> grid_in;
+         grid_in.attach_triangulation(triangulation);
+         std::ifstream input_file("naca_struct.msh");
+         grid_in.read_msh(input_file);
+         n_refine = 0;
+      }
+      
+      // Attach manifold to boundaries
+      if(test_case==0 || test_case==1 || test_case==2)
+      {
+         static const SphericalManifold<2> boundary;
+         triangulation.set_all_manifold_ids_on_boundary(0);
+         triangulation.set_manifold (0, boundary);
+      }
+      else  if(test_case==3)
+      {
+         NACA::set_curved_boundaries (triangulation);
+         n_refine = 0;
+      }
+      
+      // Do some refinement
+      if(n_refine > 0) triangulation.refine_global(n_refine);
+      
+      // Compute the euler vector
+      unsigned int degree = 4;
+      compute_mapping (degree, triangulation);
    }
-   else if(test_case==1)
+   catch (std::exception &exc)
    {
-      GridGenerator::hyper_shell (triangulation, Point<2>(0.0,0.0), 0.5, 1.0);
-      n_refine = 2;
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Exception on processing: " << std::endl
+                << exc.what() << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
    }
-   else if(test_case==2)
+   catch (...)
    {
-      GridIn<2> grid_in;
-      grid_in.attach_triangulation(triangulation);
-      std::ifstream input_file("annulus.msh");
-      grid_in.read_msh(input_file);
-      n_refine = 0;
-   }
-   else if(test_case==3)
-   {
-      GridIn<2> grid_in;
-      grid_in.attach_triangulation(triangulation);
-      std::ifstream input_file("naca_struct.msh");
-      grid_in.read_msh(input_file);
-      n_refine = 0;
-   }
-   
-   // Attach manifold to boundaries
-   if(test_case==0 || test_case==1 || test_case==2)
-   {
-      static const SphericalManifold<2> boundary;
-      triangulation.set_all_manifold_ids_on_boundary(0);
-      triangulation.set_manifold (0, boundary);
-   }
-   else  if(test_case==3)
-   {
-      NACA::set_curved_boundaries (triangulation);
-      n_refine = 0;
-   }
-   
-   // Do some refinement
-   if(n_refine > 0) triangulation.refine_global(n_refine);
-   
-   // Compute the euler vector
-   unsigned int degree = 4;
-   compute_mapping (degree, triangulation);
+      std::cerr << std::endl << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      std::cerr << "Unknown exception!" << std::endl
+                << "Aborting!" << std::endl
+                << "----------------------------------------------------"
+                << std::endl;
+      return 1;
+   };
    
    return 0;
 }
