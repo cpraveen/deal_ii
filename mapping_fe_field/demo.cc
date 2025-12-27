fcd340a4c6cc14e9511164a6da2230e44ca34a3a
Praveen C <cpraveen@gmail.com>
2025-12-27 09:19:17 +0530

    Add another map

--- mapping_fe_field/demo.cc
+++ mapping_fe_field/demo.cc
@@ -19,11 +19,13 @@ using namespace dealii;
 const int dim = 2;
 
 //-----------------------------------------------------------------------------
+// Taken from Arpit thesis
+//-----------------------------------------------------------------------------
 template <int dim>
-class MyMap : public Function<dim>
+class MyMap0 : public Function<dim>
 {
    public:
-      MyMap() : Function<dim>(dim) {}
+      MyMap0() : Function<dim>(dim) {}
 
       void vector_value(const Point<dim>& p, 
                         Vector<double>& value) const override
@@ -42,9 +44,37 @@ class MyMap : public Function<dim>
 };
 
 //-----------------------------------------------------------------------------
-int main()
+// Taken from Arpit thesis
+// Modified to map [0,1]x[0,1] --> [0,1]x[0,1]
+//-----------------------------------------------------------------------------
+template <int dim>
+class MyMap1 : public Function<dim>
+{
+   public:
+      MyMap1() : Function<dim>(dim) {}
+
+      void vector_value(const Point<dim>& p, 
+                        Vector<double>& value) const override
+      {
+         const double xi = 3.0 * p[0];
+         const double eta = 3.0 * p[1];
+         const double y = eta + 3.0/8.0 * (cos(1.5 * M_PI * (2 * xi - 3)/3) *
+                                           cos(0.5 * M_PI * (2 * eta - 3)/3));
+         const double x = xi + 3.0/8.0 * (cos(0.5 * M_PI * (2 * xi - 3)/3) *
+                                          cos(2 * M_PI * (2 * y - 3)/3));
+         value[0] = x/3.0;
+         value[1] = y/3.0;
+      }
+};
+
+//-----------------------------------------------------------------------------
+int main(int argc, char* argv[])
 {
    const int degree = 2;
+
+   int test = 0;
+   if(argc == 2) test = Utilities::string_to_int(argv[1]);
+   std::cout << "Test case = " << test << std::endl;
 
    const FESystem<dim> fe(FE_Q<dim>(degree), dim);
 
@@ -56,7 +86,10 @@ int main()
    dof_handler.distribute_dofs(fe);
 
    Vector<double> euler(dof_handler.n_dofs());
-   VectorTools::interpolate(dof_handler, MyMap<dim>(), euler);
+   if(test == 0)
+      VectorTools::interpolate(dof_handler, MyMap0<dim>(), euler);
+   else
+      VectorTools::interpolate(dof_handler, MyMap1<dim>(), euler);
    MappingFEField<dim,dim> mapping(dof_handler, euler);
 
    // Original grid
