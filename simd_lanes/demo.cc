6cd84de6b33ab6ab5d11c9cb352dfe0c1960dc3f
Praveen C <cpraveen@gmail.com>
2026-07-17 13:03:35 +0530

    Simplify simd test

--- simd_lanes/demo.cc
+++ simd_lanes/demo.cc
@@ -1,38 +1,11 @@
-#include <deal.II/base/quadrature_lib.h>
-#include <deal.II/dofs/dof_handler.h>
-#include <deal.II/fe/fe_q.h>
-#include <deal.II/fe/mapping_q1.h>
-#include <deal.II/grid/grid_generator.h>
-#include <deal.II/grid/tria.h>
-#include <deal.II/lac/affine_constraints.h>
-#include <deal.II/matrix_free/matrix_free.h>
-
+#include <deal.II/base/vectorization.h>
 #include <iostream>
 
 using namespace dealii;
 
-int main() {
-  constexpr unsigned int dim = 2;
-
-  Triangulation<dim> triangulation;
-  GridGenerator::hyper_cube(triangulation);
-  triangulation.refine_global(1);
-
-  FE_Q<dim> fe(1);
-  DoFHandler<dim> dof_handler(triangulation);
-  dof_handler.distribute_dofs(fe);
-
-  AffineConstraints<double> constraints;
-  constraints.close();
-
-  MappingQ1<dim> mapping;
-
-  MatrixFree<dim, double> matrix_free;
-  matrix_free.reinit(mapping, dof_handler, constraints, QGauss<1>(2));
-
-  unsigned int n_lanes = matrix_free.n_active_entries_per_cell_batch(0);
-  std::cout << n_lanes << " lanes mf\n";
-
+int main()
+{
+  std::cout << "SIMD width = " << VectorizedArray<double>::size() << "\n";
   return 0;
 }
 
